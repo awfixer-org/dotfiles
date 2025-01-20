86cabdc6eeb98337c4e7b13d742328768f7e8ff1
awfixer <awfixer@pm.me>
2025-01-19 17:14:14 -0800

    zsh did it for me

--- setup.sh
+++ setup.sh
@@ -1,39 +1,48 @@
-#!/bin/bash
-
 rm -rf ~/.vim && ln -s ~/dotfiles/vim/.vim ~/.vim
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.zshrc && ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+rm -rf ~/.zshenv && ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
 
-rm -rf ~/.config/nvim && ln -s ~/dotfiles/nvim/ ~/.config/nvim
+rm -rf ~/.tmux.conf && ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+rm -rf ~/.tmux.conf.local && ln -s ~/dotfiles/tmux/.tmux.conf.local ~/.tmux.conf.local
+rm -rf ~/.tmux && ln -s ~/dotfiles/tmux/.tmux ~/.tmux
 
-rm -rf ~/.config/hypr && ln -s ~/dotfiles/hypr ~/.config/hypr
+rm -rf ~/.oh-my-zsh && ln -s ~/dotfiles/oh-my-zsh/ ~/.oh-my-zsh
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/anyrun/ && ln -s ~/dotfiles/anyrun/ ~/.config/anyrun/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/cordless/ && ln -s ~/dotfiles/cordless/ ~/.config/cordless/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/doom/ && ln -s ~/dotfiles/doom/ ~/.config/doom/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/emacs/ && ln -s ~/dotfiles/emacs/ ~/.config/emacs/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/fontconfig/ && ln -s ~/dotfiles/fontconfig/ ~/.config/fontconfig/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/ghostty/ && ln -s ~/dotfiles/ghostty/ ~/.config/ghostty/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/guix/ && ln -s ~/dotfiles/guix/ ~/.config/guix/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/hypr/ && ln -s ~/dotfiles/hypr/ ~/.config/hypr/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/hyprdock/ ln -s ~/dotfiles/hyprdock/ ~/.config/hyprdock/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/hyprpanel/ && ln -s ~/dotfiles/hyprpanel/ ~/.config/hyprpanel/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/neofetch/ && ln -s ~/dotfiles/neofetch/ ~/.config/neofetch/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/nvim/ && ln -s ~/dotfiles/nvim/ ~/.config/nvim/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+um -rf ~/.config/nwg-dock-hyprland/ && ln -s ~/dotfiles/nwg-dock-hyprland/ ~/.config/nwg-dock-hyprland/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/nwg-drawer/ && ln -s ~/dotfiles/nwg-drawer/ ~/.config/nwg-drawer/
 
-rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+rm -rf ~/.config/pacseek/ && ln -s ~/dotfiles/pacseek/ ~/.config/pacseek/
+
+rm -rf ~/.config/thefuck/ && ln -s ~/dotfiles/thefuck/ ~/.config/thefuck/
+
+rm -rf ~/.config/Thunar/ && ln -s ~/dotfiles/Thunar/ ~/.config/Thunar/
+
+rm -rf ~/.config/wlogout/ && ln -s ~/dotfiles/wlogout/ ~/.config/wlogout/
+
+rm -rf ~/.config/zellij/ && ln -s ~/dotfiles/zellij/ ~/.config/zellij/
---
+++ setup.sh.bak
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+rm -rf ~/.vim && ln -s ~/dotfiles/vim/.vim ~/.vim
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.config/nvim && ln -s ~/dotfiles/nvim/ ~/.config/nvim
+
+rm -rf ~/.config/hypr && ln -s ~/dotfiles/hypr ~/.config/hypr
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
+
+rm -rf ~/.vimrc && ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
